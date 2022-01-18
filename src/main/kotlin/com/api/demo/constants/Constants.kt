009210f1a5725a6f8417be777c3c03aa4796b216
Antoine Boucher <antoine.bou13@gmail.com>
2022-01-18 15:36:27 -0500

    Update Constants.kt

--- src/main/kotlin/com/api/demo/constants/Constants.kt
+++ src/main/kotlin/com/api/demo/constants/Constants.kt
@@ -1,4 +1,4 @@
 package com.api.demo.constants
 
-const val MAX_DOCUMENT_FILE_SIZE = 50000 * 1024 * 1024 //50000 MB
+const val MAX_DOCUMENT_FILE_SIZE = 5000000 * 1024 * 1024 //5000000 MB
 const val DOC_FILE_EXTENSION_REGEX = "(.+(\\.(?i)(xls|xlsx|docx|doc|pdf|jpeg|png|gif|bmp|jpg|mp4|mov|zip))\$)"
